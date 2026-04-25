816641115432ae9ee155d77875ccec194838700e
RAKOTONDRATREMA Sarobidinirina Fanantenana <sarobidinirinarakoto@gmail.com>
2026-04-25 19:55:49 +0300

    Update autre_fonction.h

--- autre_fonction.h
+++ autre_fonction.h
@@ -2,3 +2,4 @@ float calcul(std::string fonction,float x);
 float calcul_fonction(std::string fonction,float x);
 float calcul_entre_parenthese(std::string fonction,double x);
 float _fonction(std::string fonction ,float x);
+float _limite(std::string fonction ,float a);
