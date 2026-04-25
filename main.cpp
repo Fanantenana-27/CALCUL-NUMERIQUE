d4d7c95379baa60b1c9f3ae084362f7ec9766488
RAKOTONDRATREMA Sarobidinirina Fanantenana <sarobidinirinarakoto@gmail.com>
2026-04-25 21:23:03 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include "Fonction.h"
+#include <iostream>
 
 int main()
 {
+    float a , b;
+    std::string fonction;
     Fonction f = Fonction();
-    f.setFonction("3x^2-7");
+    std::cout << "Entrez une fonction pôlunome : ";
+    std::cin >> fonction;
+    std :: cout << "Entrez les bornes a et b  de l'interval [a,b] : \na = ";
+    std::cin >> a;
+    std::cout << "b = ";
+    std::cin >> b;
+    f.setFonction(fonction);
     f.f(-1);
-    f.newton(1,5);
-    f.dichotomie(1,7);
+    f.newton(a,b);
+    f.dichotomie(a,b);
     f.lim(0);
     f.df(3);
     return(0);
