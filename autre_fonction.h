cd4bd19af8379a21b10d1d13b34bc89caf041ec6
RAKOTONDRATREMA Sarobidinirina Fanantenana <sarobidinirinarakoto@gmail.com>
2026-04-25 19:55:01 +0300

    Add new function declaration for _fonction

--- autre_fonction.h
+++ autre_fonction.h
@@ -1,3 +1,4 @@
 float calcul(std::string fonction,float x);
 float calcul_fonction(std::string fonction,float x);
 float calcul_entre_parenthese(std::string fonction,double x);
+float _fonction(std::string fonction ,float x);
